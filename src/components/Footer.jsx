1fa08145865323e96c7cc206a21e8a12031214be
Hinano802 <chhi22083@g.nihon-u.ac.jp>
2023-12-04 15:29:12 +0900

    基盤

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>5422083　古賀日南乃</p>
+      <p>日本大学文理学部情報科学科 Webプログラミングの演習課題</p>
+    </footer>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <p>サイトの中身</p>
+    </main>
+  );
+}
